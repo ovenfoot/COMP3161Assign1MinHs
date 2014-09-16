b035329651718b9066a030fa71a8a2a1cc4df097
tnguyenbluesat <tnguyen@bluesat.unsw.edu.au>
2014-09-16 20:04:02 +1000

    recursion SORT OF working

--- MinHS/Evaluator.hs
+++ MinHS/Evaluator.hs
@@ -44,17 +44,13 @@ evalE env (App (Prim Neg) e1 )= evalE env (App (Prim Neg) (Num n)) where
 evalE env (App (App (Prim Add) (Num n)) (Num m)) =  I (n+m)
 evalE env (App (App (Prim Sub) (Num n)) (Num m)) =  I (n-m)
 evalE env (App (App (Prim Quot) (Num n)) (Num m)) = I(quot n m)
-
 evalE env (App (App (Prim Mul) (Num n)) (Num m)) = (I(n*m))
 evalE env (App (App (Prim Eq) (Num n)) (Num m)) = B (n==m)
 evalE env (App (App (Prim Ne) (Num n)) (Num m)) = B (n/=m)
 evalE env (App (App (Prim Gt) (Num n)) (Num m)) = B (n>m)
-
 evalE env (App (App (Prim Ge) (Num n)) (Num m)) = B (n>=m)
 evalE env (App (App (Prim Lt) (Num n)) (Num m)) = B (n<m)
 evalE env (App (App (Prim Le) (Num n)) (Num m)) = B (n<=m)
-
-
 
 --isempty
 evalE env (App (Prim Null) (Con "Nil")) = B True
@@ -62,15 +58,15 @@ evalE env (App (Prim Null) (Var id)) = evalE env (App (Prim Null) v) where v = d
 
 evalE env (App (Prim Null) e1) = B False
 
+evalE env (App (App (Prim p) e1) (e2)) = evalE env (App (App (Prim p) (Num n)) (Num m)) where I n = evalE env e1; I m = evalE env e2
+
+
+--If else
 evalE env (If (Con "True") e1 e2) = evalE env e1
 evalE env (If (Con "False") e1 e2) = evalE env e2
-
 evalE env (If e1 e2 e3) = evalE env (If tf e2 e3) where
 			tf = devalV (evalE env e1)
 
-
-
-evalE env (App (App (Prim p) e1) (e2)) = evalE env (App (App (Prim p) (Num n)) (Num m)) where I n = evalE env e1; I m = evalE env e2
 
 
 --listops
@@ -85,10 +81,10 @@ evalE env (App (Prim Head) (App e1 e2)) = evalE env (App (Prim Head) e1)
 evalE env (App (Prim Tail) (Con "Nil"))  = error("Cannot retrieve tail from empty List.")
 evalE env (App (Prim Tail)(App (App (Con "Cons") (Num n)) (Con "Nil"))) = Cons n Nil
 evalE env (App (Prim Tail) (App (App (Con "Cons") (Num n)) e2)) = evalE env (App (Prim Tail) e2) --remove the head
---evalE env (App (Prim Tail) (App e1 e2)) = Cons (evalInt(evalE env (App (Prim Head) e1))) (evalE env (App (Prim Tail) e2))
-
 evalE env (App (Prim Tail) (App e1 e2)) = Cons n (evalE env (App (Prim Tail) e2)) where
    I n = evalE env (App (Prim Head) e1)
+
+--evalE g e = error("Unimplented, environment is -->" ++(show g)++ "<-- exp is -->" ++(show e)++"<--")
 
 --Letcases (and letfun cases)
 --Bind the closure from Letfun into funcname
@@ -96,7 +92,7 @@ evalE env (Let [Bind funcname1 _ _ (Letfun b1)] funcapp) = evalE (E.add (env) (f
 
 evalE env (Letfun (Bind funcname typ [vars] funcexp)) = Close env' (Letfun b') where
 		b' = (Bind funcname typ [vars] funcexp);
-		env' = E.add (env) (funcname, Close env (Letfun b'))
+		env' = env --E.add (env) (funcname, Close env (Letfun b'))
 
 
 evalE env (App (Letfun (Bind funcname typ [vars] funcexp)) e1) = evalE (E.add (env) (vars, I n)) funcexp where
@@ -120,8 +116,14 @@ evalE env (Var id) =
                            
            
 --Recursion
+--evalE env (App (Var id1) (Var id2)) = error("foundsomething")
+--evalE env (App  e1 e2) = error("starting recursion on -->" ++(show e1)++"<--- and -->"++(show e2))
+--evalE env (App  e1 e2) = evalE env (App (devalV (evalE env e1)) (devalV (evalE env e2)))
 
-evalE env (App  e1 e2) = evalE env (App (devalV (evalE env e1)) (devalV (evalE env e2)))
+evalE env (App  e1 e2) = 
+	case (evalE env e1) of
+		Close env' e1'  -> evalE env' (App e1' e2)	
+		_				-> error("SHIT NIGGA")
 --Functions
 --evalE env (Letfun (Bind typ x e) = 
 
@@ -135,6 +137,7 @@ devalV(I n) = Num n
 devalV(Nil) = Con "Nil"
 devalV(B True) = Con "True"
 devalV(B False) = Con "False" 
+--devalV (Close env exp) = exp
 devalV e = error("devalV not implemented for -->"++(show e)++"<----");
 
 
--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ cabal build
 #for file in tests/task1/3_variables/*.mhs; do
 #   ./dist/build/minhs-1/minhs-1 $file
 #done
-./dist/build/minhs-1/minhs-1 tests/task1/5_let/1_functions/003.mhs
-
-
+#./dist/build/minhs-1/minhs-1 tests/task1/5_let/1_functions/005.mhs
+./dist/build/minhs-1/minhs-1 smallrecursion.mhs
